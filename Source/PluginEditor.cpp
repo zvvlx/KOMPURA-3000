6e93b703b9aa750a7e24130fba5f17f3f9738053
zvvlx <samu@paasky.fi>
2022-05-24 15:35:00 +0300

    change window size and replace hello world text with a proper plug-in name

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ KOMPURA3000AudioProcessorEditor::KOMPURA3000AudioProcessorEditor (KOMPURA3000Aud
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (1000, 500);
 }
 
 KOMPURA3000AudioProcessorEditor::~KOMPURA3000AudioProcessorEditor()
@@ -30,7 +30,7 @@ void KOMPURA3000AudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("KOMPURA-3000", getLocalBounds(), juce::Justification::centredTop, 1);
 }
 
 void KOMPURA3000AudioProcessorEditor::resized()
