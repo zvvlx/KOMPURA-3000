cd2b226f81b09657ee843c43cc34e84565410f71
zvvlx <samu@paasky.fi>
2022-05-27 13:35:30 +0300

    remove unused code

--- Source/Compressor.cpp
+++ Source/Compressor.cpp
@@ -24,19 +24,6 @@ float Compressor::compressSample(float data, float thresh, float ratio, float at
     float dbRMS = 10 * std::log10(rms);
 
     float slope = 1 - (1 / ratio);
-
-    if (knee > 0 && dbRMS > (thresh - knee / 2.0) && dbRMS < (thresh + knee / 2.0)) {
-        float kneeBottom = thresh - knee / 2.0, kneeTop = thresh + knee / 2.0;
-        float xPoints[2], yPoints[2];
-        xPoints[0] = kneeBottom;
-        xPoints[1] = kneeTop;
-        xPoints[1] = std::fmin(0.0f, kneeTop);
-        yPoints[0] = 0.0f;
-        yPoints[1] = slope;
-        slope = interpolatePoints(&xPoints[0], &yPoints[0], thresh);
-        thresh = kneeBottom;
-    }
-
     float dbGain = std::min(0.0f, (slope * (thresh - dbRMS)));
     float newGain = std::pow(10, dbGain / 20);
 
@@ -50,24 +37,4 @@ float Compressor::compressSample(float data, float thresh, float ratio, float at
     buffer.setData(data);
     buffer.nextSample();
     return compressedSample;
-}
-
-float Compressor::interpolatePoints(float* xPoints, float* yPoints, float detectedValue) {
-    float result = 0.0f;
-    int n = 2;
-
-    for (int i = 0; i < n; i++)
-    {
-        float term = 1.0f;
-        for (int j = 0; j < n; j++)
-        {
-            if (j != i) {
-                term *= (detectedValue - xPoints[j]) / (xPoints[i] - xPoints[j]);
-            }
-        }
-        result += term * yPoints[i];
-    }
-
-    return result;
-
 }
